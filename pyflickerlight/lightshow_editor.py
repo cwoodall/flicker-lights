35c280c9f917dacb3ef111a57474a7c77f5449c0
Christopher Woodall <chris.j.woodall@gmail.com>
2016-11-26 13:31:21 -0500

    Added scrolling and artist and title.

--- pyflickerlight/lightshow_editor.py
+++ pyflickerlight/lightshow_editor.py
@@ -1,8 +1,19 @@
 import sys
 import yaml
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from lightshow_gui import Ui_MainWindow
+import matplotlib
+import wave
+import numpy as np
+from tinytag import TinyTag
+matplotlib.use("Qt5Agg")
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import matplotlib.pyplot as plt
+
 import math
+import random
+
+import threading
 
 class FlickerLightItem(QtWidgets.QGraphicsItem):
     def __init__ (self, position, color, offset_y=10, width=10, height=30):
@@ -27,7 +38,6 @@ class FlickerLightItem(QtWidgets.QGraphicsItem):
 
     def mousePressEvent(self, event):
         super(FlickerLightItem, self).mousePressEvent(event)
-        print ("YOU CLICKED ME!")
 
 class TickPainter(object):
     def __init__(self, tickDistance, numTicksPerMajorTick, minorLen=5, majorLen=8, tickColor=QtGui.QColor(50,50,50,125)):
@@ -67,6 +77,8 @@ class TickPainter(object):
 class MyGraphicsScene(QtWidgets.QGraphicsScene):
     def __init__(self, tickDistance=10, numTicksPerMajorTick=10):
         QtWidgets.QGraphicsScene.__init__(self)
+        self.tickDistance = tickDistance
+        self.numTicksPerMajorTick = numTicksPerMajorTick
         self.tickPainter = TickPainter(tickDistance, numTicksPerMajorTick)
         self.currentColor = QtGui.QColor(0,0,0,255)
         self.shortcut_colors = [self.currentColor for i in range(10)]
@@ -106,7 +118,7 @@ class MyGraphicsScene(QtWidgets.QGraphicsScene):
         super(MyGraphicsScene, self).mousePressEvent(event)
         position = QtCore.QPointF(event.scenePos())
         tick_position = self.tickPainter.getPosition(position)
-        item = FlickerLightItem(tick_position, self.currentColor)
+        item = FlickerLightItem(tick_position, self.currentColor, width=self.tickDistance)
         collidingItems = self.collidingItems(item, QtCore.Qt.IntersectsItemShape)
         if not collidingItems:
             self.addItem(item)
@@ -116,18 +128,75 @@ class MyGraphicsScene(QtWidgets.QGraphicsScene):
 
 class LightshowProgram(QtWidgets.QMainWindow):
     def setupGraphicsScene(self):
-            self.sceneLength = self.tickDistance * (self.songLength/self.timePerTick)
-            self.scene = MyGraphicsScene(self.tickDistance, 1/self.timePerTick)
-            self.testCanvas.setScene(self.scene)
-            self.testCanvas.setSceneRect(0,0,self.sceneLength,50)
+        self.sceneLength = self.tickDistance * (self.songLength/self.timePerTick)
+        self.scene = MyGraphicsScene(self.tickDistance, 1/self.timePerTick)
+        self.testCanvas.setScene(self.scene)
+        self.testCanvas.setSceneRect(0,0,self.sceneLength,50)
+
+    def setupPlot(self):
+        # a figure instance to plot on
+        self.figure = plt.figure()
+
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
+
+        self.plottingLayout.addWidget(self.canvas)
+
+    def plot(self, event=None):
+        ''' plot some random stuff '''
+        N = 516
+
+        if self.currentSong:
+            if self.loadedSong != self.currentSong:
+                spf = wave.open(self.currentSong,'r')
+                self.fs = spf.getframerate()
+                self.signal = spf.readframes(-1)
+                self.signal = np.fromstring(self.signal, 'Int16')
+                self.loadedSong = self.currentSong
+                self.signa_compressed = self.signal[0:-1:2] # make Mono
+                self.signa_compressed = self.signa_compressed[0:-1:N] # Resample
+
+            orig_len = len(self.signal)
+
+            #/2 to account for the stero to mono conversion
+            Time=np.linspace(0,orig_len/float(self.fs)/2, num=len(self.signa_compressed))
+
+            # create an axis
+            ax = self.figure.add_subplot(111)
+            # discards the old graph
+            ax.hold(False)
+
+            scene_rect = self.testCanvas.mapToScene(self.testCanvas.viewport().rect()).boundingRect()
+
+            point_to_time = lambda x, w, dt: dt*x/w
+            time_start = point_to_time(scene_rect.left(), self.tickDistance, self.timePerTick)
+            num_secs = point_to_time(scene_rect.width(), self.tickDistance, self.timePerTick)
+
+            dt = Time[1] - Time[0]
+            num = int(num_secs / dt)
+            point_start = int(time_start / dt)
+            # plot data
+            ax.plot(Time[point_start:point_start+num], self.signa_compressed[point_start:point_start+num])
+
+            ax.set_xlabel("Time (s)")
+            ax.get_yaxis().set_visible(False)
+            self.figure.tight_layout()
+            self.figure.subplots_adjust(left=0, right=1, top=1, bottom=0.2)
+            ax.relim()
+            ax.autoscale_view(True,True,True)
+            # refresh canvas
+            self.canvas.draw()
 
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         uic.loadUi('lightshow_editor.ui', self)
         # self.setupUi(window)
-        self.songLength = 60*1
-        self.timePerTick = .25
-        self.tickDistance = 10
+        self.songLength = 0
+        self.timePerTick = .1
+        self.tickDistance = 5
+        self.currentSong = None
+        self.loadedSong = None
 
         self.setupGraphicsScene()
 
@@ -135,6 +204,29 @@ class LightshowProgram(QtWidgets.QMainWindow):
         self.playButton.setText("Save SHOW File")
         self.pauseButton.clicked.connect(self.load_show_yaml)
         self.pauseButton.setText("Load SHOW File")
+        self.loadSongButton.clicked.connect(self.loadNewSong)
+        self.setupPlot()
+        self.testCanvas.horizontalScrollBar().valueChanged.connect(self.plot)
+        self.resizeEvent = lambda event: self.plot()
+
+    def updateSongInfo(self, title, artist, duration):
+        self.titleLabel.setText(title)
+        self.artistLabel.setText(artist)
+        self.songLength = duration
+        self.durationLabel.setText("{:.2f} seconds".format(duration))
+
+    def updateSong(self, file_name):
+        self.filenameLabel.setText(file_name)
+        self.currentSong = file_name
+        tag = TinyTag.get(file_name)
+        self.updateSongInfo(tag.title, tag.artist, tag.duration)
+        self.setupGraphicsScene()
+        self.plot()
+
+    def loadNewSong(self):
+        file_dialog = QtWidgets.QFileDialog.getOpenFileName()
+        if file_dialog[0]:
+            self.updateSong(file_dialog[0])
 
     def load_show_yaml(self):
         file_dialog = QtWidgets.QFileDialog.getOpenFileName()
@@ -142,6 +234,10 @@ class LightshowProgram(QtWidgets.QMainWindow):
             show_config = {}
             with open(file_dialog[0],'r') as yaml_file:
                 show_config = yaml.load(yaml_file)
+
+            self.updateSong(show_config["location"])
+
+            self.updateSongInfo(show_config["title"], show_config["artist"], self.songLength)
 
             # Clear current canvas
             items = sorted(self.scene.items(), key = lambda item: item.position)
@@ -153,6 +249,7 @@ class LightshowProgram(QtWidgets.QMainWindow):
                 color = show_time_item['commands'][0]['color']
                 item = FlickerLightItem(position, QtGui.QColor((color&0xFF0000)>>16, (color & 0x00FF00)>>8, color & 0x0000FF))
                 self.scene.addItem(item)
+
     def save_show_yaml(self):
         file_dialog = QtWidgets.QFileDialog.getSaveFileName()
         if file_dialog[0]:
@@ -168,9 +265,9 @@ class LightshowProgram(QtWidgets.QMainWindow):
                         'color': int(item.color.name().replace("#", ""),16)}]}
                 show.append(show_command)
             metadata = {
-                "title": "Yellow Flicker Beat",
-                "artist": "Lorde",
-                "location": "assets/song.wav",
+                "title": self.titleLabel.text(),
+                "artist": self.artistLabel.text(),
+                "location": self.currentSong,
                 "date": "2016-09-10",
                 "start_time": 0,
                 "speed": 1,
--- pyflickerlight/mygraphicsview.py
+++ pyflickerlight/mygraphicsview.py
@@ -11,19 +11,28 @@ class MyGraphicsView(QtWidgets.QGraphicsView):
         """
         Zoom in or out of the view.
         """
-        zoomInFactor = 1.1
-        zoomOutFactor = 1 / zoomInFactor
+        modifiers = QtWidgets.QApplication.keyboardModifiers()
+        if modifiers == QtCore.Qt.ControlModifier:
+            zoomInFactor = 1.1
+            zoomOutFactor = 1 / zoomInFactor
 
-        # Save the scene pos
-        oldPos = self.mapToScene(event.pos())
+            # Save the scene pos
+            oldPos = self.mapToScene(event.pos())
 
-        # Zoom
-        if event.angleDelta().y() > 0:
-            zoomFactor = zoomInFactor
+            # Zoom
+            if event.angleDelta().y() > 0:
+                zoomFactor = zoomInFactor
+            else:
+                zoomFactor = zoomOutFactor
+
+            if ((self.currentScale * zoomFactor)>= 1) and (self.currentScale * zoomFactor <= 100):
+                self.currentScale *= zoomFactor
+                self.scale(zoomFactor, 1)
         else:
-            zoomFactor = zoomOutFactor
-        print(self.currentScale)
-        if ((self.currentScale * zoomFactor)>= 1):
-            print (self.currentScale)
-            self.currentScale *= zoomFactor
-            self.scale(zoomFactor, 1)
+            if event.angleDelta().y() > 0:
+                dx = 25
+            else:
+                dx = -25
+
+            x = self.horizontalScrollBar().value()
+            self.horizontalScrollBar().setValue(x + dx)
